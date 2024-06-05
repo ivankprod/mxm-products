fe3866ad17c1b8a75fb359e9c03a4e221645de38
ivankprod <houseprotector@mail.ru>
2024-06-05 21:14:07 +0500

    feat(components): local products table sorting, filter and pagination implemented

--- src/components/localProductsList/localProductsList.tsx
+++ src/components/localProductsList/localProductsList.tsx
@@ -1,8 +1,21 @@
 "use client";
 
-import { useState } from "react";
-import { Button, Flex, Select, Space, Switch, Table, Tag, Tooltip } from "antd";
-import { DeleteOutlined, EditOutlined, PlusOutlined } from "@ant-design/icons";
+import { useRef, useState } from "react";
+import Highlighter from "react-highlight-words";
+import {
+	Button,
+	Flex,
+	Input,
+	InputRef,
+	Select,
+	Space,
+	Switch,
+	Table,
+	TableColumnType,
+	Tag,
+	Tooltip
+} from "antd";
+import { DeleteOutlined, EditOutlined, PlusOutlined, SearchOutlined } from "@ant-design/icons";
 
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { PerPageOptions } from "@/lib/constants";
@@ -10,6 +23,9 @@ import { updateProduct, deleteProduct, addProduct } from "@/lib/features/local";
 import { TProduct, statusPublished, statusUnpublished } from "@/types/product";
 
 import styles from "./localProductsList.module.css";
+import { FilterDropdownProps } from "antd/es/table/interface";
+
+type DataIndex = keyof TProduct;
 
 export const LocalProductsList: React.FC = () => {
 	const [shown, setShown] = useState(PerPageOptions[0].value as number);
@@ -18,11 +34,99 @@ export const LocalProductsList: React.FC = () => {
 	const products = useAppSelector((state) => state.local);
 	const dispatch = useAppDispatch();
 
+	const [searchText, setSearchText] = useState("");
+	const [searchedColumn, setSearchedColumn] = useState("");
+	const searchInput = useRef<InputRef>(null);
+
 	const productsFilteredByStatus = filteredByStatus
 		? products.filter((product) =>
 				product.status ? product.status === statusPublished : false
 		  )
 		: products;
+
+	const handleSearch = (
+		selectedKeys: string[],
+		confirm: FilterDropdownProps["confirm"],
+		dataIndex: DataIndex
+	) => {
+		confirm();
+		setSearchText(selectedKeys[0]);
+		setSearchedColumn(dataIndex);
+	};
+
+	const handleReset = (clearFilters: () => void) => {
+		clearFilters();
+		setSearchText("");
+	};
+
+	const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<TProduct> => ({
+		filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+			<div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+				<Input
+					ref={searchInput}
+					placeholder={`Search ${dataIndex}`}
+					value={selectedKeys[0]}
+					onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+					onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+					style={{ marginBottom: 8, display: "block" }}
+				/>
+				<Space>
+					<Button
+						type="primary"
+						onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+						icon={<SearchOutlined />}
+					>
+						Найти
+					</Button>
+					<Button onClick={() => clearFilters && handleReset(clearFilters)}>
+						Очистить
+					</Button>
+					<Button
+						type="link"
+						onClick={() => {
+							confirm();
+							setSearchText((selectedKeys as string[])[0]);
+							setSearchedColumn(dataIndex);
+						}}
+					>
+						Применить
+					</Button>
+					<Button
+						type="link"
+						size="middle"
+						onClick={() => {
+							close();
+						}}
+					>
+						Закрыть
+					</Button>
+				</Space>
+			</div>
+		),
+		filterIcon: (filtered: boolean) => (
+			<SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
+		),
+		onFilter: (value, record) =>
+			record[dataIndex]!.toString()
+				.toLowerCase()
+				.includes((value as string).toLowerCase()),
+		onFilterDropdownOpenChange: (visible) => {
+			if (visible) {
+				setTimeout(() => searchInput.current?.select(), 100);
+			}
+		},
+		render: (text) =>
+			searchedColumn === dataIndex ? (
+				<Highlighter
+					highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+					searchWords={[searchText]}
+					autoEscape
+					textToHighlight={text ? text.toString() : ""}
+				/>
+			) : (
+				text
+			)
+	});
 
 	return (
 		<Flex className={styles["cards-container"]} vertical gap="large">
@@ -35,14 +139,15 @@ export const LocalProductsList: React.FC = () => {
 					<Button
 						type="primary"
 						icon={<PlusOutlined />}
-						onClick={() => { //mock
+						onClick={() => {
+							//mock
 							dispatch(
 								addProduct({
-									id: 1,
-									key: "3",
+									id: Math.floor(Math.random() * 100),
+									key: String(Math.floor(Math.random() * 100)),
 									title: "Test",
-									description: "Test desc",
-									price: 100,
+									description: "Test descfdddf",
+									price: Math.floor(Math.random() * 1000),
 									status: statusUnpublished
 								})
 							);
@@ -71,31 +176,64 @@ export const LocalProductsList: React.FC = () => {
 					/>
 				</Flex>
 			</Flex>
-			<Table dataSource={productsFilteredByStatus} style={{ overflowX: "auto" }}>
-				<Table.Column title="ID" dataIndex="id" key="id" />
-				<Table.Column
+			<Table
+				dataSource={productsFilteredByStatus}
+				showSorterTooltip={{
+					title: "Нажмите для сортировки",
+					overlayStyle: { fontSize: 14 }
+				}}
+				pagination={{
+					position: ["bottomCenter"],
+					hideOnSinglePage: true,
+					showTotal: (total, range) => `${range[0]}-${range[1]} из ${total} записей`,
+					pageSize: shown
+				}}
+				style={{ overflowX: "auto" }}
+			>
+				<Table.Column<TProduct>
+					title="ID"
+					dataIndex="id"
+					key="id"
+					defaultSortOrder="ascend"
+					sorter={(a, b) => a.id - b.id}
+				/>
+				<Table.Column<TProduct>
 					title="Название"
 					dataIndex="title"
 					key="title"
+					{...getColumnSearchProps("title")}
 				/>
-				<Table.Column title="Цена" dataIndex="price" key="price" />
-				<Table.Column title="Описание" dataIndex="description" key="description" />
-				<Table.Column
+				<Table.Column<TProduct>
+					title="Цена"
+					dataIndex="price"
+					key="price"
+					sorter={(a, b) => a.price - b.price}
+				/>
+				<Table.Column<TProduct>
+					title="Описание"
+					dataIndex="description"
+					key="description"
+					{...getColumnSearchProps("description")}
+				/>
+				<Table.Column<TProduct>
 					title="Статус"
 					key="status"
 					dataIndex="status"
-					render={(_, { status }: TProduct) => (
+					render={(_, { status }) => (
 						<Tag color={status?.color} key={status?.type}>
 							{status?.label.toUpperCase()}
 						</Tag>
 					)}
 				/>
-				<Table.Column
+				<Table.Column<TProduct>
 					title="Действия"
 					key="actions"
-					render={(_, product: TProduct) => (
+					render={(_, product) => (
 						<Space>
-							<Tooltip title="Редактировать" overlayInnerStyle={{ fontSize: "0.8rem" }}>
+							<Tooltip
+								title="Редактировать"
+								overlayInnerStyle={{ fontSize: "0.8rem" }}
+							>
 								<Button
 									onClick={() => {
 										dispatch(updateProduct(product));
